662c625a7ee35ae2aa744140a6d12595fff06d83
Alex Tkachenko <preusx.dev@gmail.com>
2015-03-14 14:57:53 +0200

    Upgraded Generator to use new Yo-API.

--- app/index.js
+++ app/index.js
@@ -2,25 +2,29 @@ var fs = require('fs');
 var path = require('path');
 
 var Generator = module.exports = function() {
-  var prompts = [];
-  var files   = this.expandFiles('**/*', { cwd: this.sourceRoot(), dot: true });
-  var ignores = [
-    '.git',
-    'LICENSE',
-    'README.md',
-  ];
+  init: function() {
+    var prompts = [];
+    var files   = this.expandFiles('**/*', { cwd: this.sourceRoot(), dot: true });
+    var ignores = [
+      '.git',
+      'LICENSE',
+      'README.md',
+    ];
 
-  this.package = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+    this.package = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
 
-  this.log.writeln('Generating from ' + 'Generator Boilerplate'.cyan + ' v' + this.package.version.cyan + '...');
+    this.log.writeln('Generating from ' + chalk.cyan('Stem Boilerplate') + ' v' + chalk.cyan(this.package.version) + '...');
 
-  files.forEach(function(file) {
-    if (ignores.indexOf(file) !== -1) {
-      return;
-    }
+    files.forEach(function(file) {
+      if (ignores.indexOf(file) !== -1) {
+        return;
+      }
 
-    this.copy(file, file);
-  }, this);
+      this.copy(file, file);
+    }, this);
+
+    this.config.save();
+  }
 };
 
-Generator.name = "Generator Boilerplate";
+Generator.name = "Stem Boilerplate";
