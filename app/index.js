b4f8cdddaa147262af355224665cc796af060fe0
Alex Tkachenko <preusx.dev@gmail.com>
2015-03-14 15:02:16 +0200

    Upgraded Generator to use new Yo-API - Fix.

--- app/index.js
+++ app/index.js
@@ -1,19 +1,29 @@
 var fs = require('fs');
 var path = require('path');
+var chalk = require('chalk');
+var generators = require('yeoman-generator');
 
-var Generator = module.exports = function() {
+var Generator = module.exports = generators.Base.extend({
   init: function() {
     var prompts = [];
-    var files   = this.expandFiles('**/*', { cwd: this.sourceRoot(), dot: true });
+    var files   = this.expandFiles('**/*', {
+      cwd: this.sourceRoot(),
+      dot: true
+    });
     var ignores = [
       '.git',
       'LICENSE',
       'README.md',
     ];
 
-    this.package = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+    this.package = JSON.parse(
+        this.readFileAsString(path.join(__dirname, '../package.json'))
+      );
 
-    this.log.writeln('Generating from ' + chalk.cyan('Stem Boilerplate') + ' v' + chalk.cyan(this.package.version) + '...');
+    this.log.writeln(
+      'Generating from ' + chalk.cyan('Stem Boilerplate') +
+      ' v' + chalk.cyan(this.package.version) + '...'
+    );
 
     files.forEach(function(file) {
       if (ignores.indexOf(file) !== -1) {
@@ -25,6 +35,6 @@ var Generator = module.exports = function() {
 
     this.config.save();
   }
-};
+});
 
 Generator.name = "Stem Boilerplate";
